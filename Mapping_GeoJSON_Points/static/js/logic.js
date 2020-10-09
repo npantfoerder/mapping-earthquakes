bc21a86d0cbde1dde904a19d37c4c81596d409bc
Neele Pantfoerder <neelesd@gmail.com>
2020-10-08 19:41:54 -0700

    Updated logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,9 +1,6 @@
 // Add console.log to check to see if the code is working
 console.log('working');
 
-// Create the map object with center and zoom level
-let map = L.map('mapid').setView([30, 30], 2);
-
 // // Add GeoJSON data
 // let sanFranAirport = {
 //     "type": "FeatureCollection",
@@ -40,7 +37,7 @@ let map = L.map('mapid').setView([30, 30], 2);
 //     }
 // }).addTo(map);
 
-// Create the tile layer that will be the background of the map
+// Create the street view tile layer 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, \
         <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, \
@@ -49,8 +46,30 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
 });
 
-// Add the 'streets' tile layer to the map
-streets.addTo(map);
+// Create the dark view tile layer
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, \
+        <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, \
+        Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/npantfoerder/mapping-earthquakes/master/majorAirports.json";
