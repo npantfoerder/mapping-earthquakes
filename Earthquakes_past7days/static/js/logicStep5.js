ff764dae6ccb2b147aea3a14c989acf7ce7c648a
Neele Pantfoerder <neelesd@gmail.com>
2020-10-11 19:04:14 -0700

    Updated logicStep5.js

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -81,9 +81,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             '<i style="background: ' + colors[i] + '"></i> ' +
             magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
         }
-  return div;
-};
-
+        return div;
     };
     legend.addTo(map);
     // Add the earthquake layer to the map
